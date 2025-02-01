fd10b0df058f1413f7d23ca7d0d07073f62a929b
aasana-lalani25 <aasanalalani25@gmail.com>
2025-02-01 09:15:26 +0530

    Update login.dart

--- login.dart
+++ login.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -7,7 +9,9 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+   MyApp({super.key});
+   TextEditingController txtemail = new TextEditingController();
+   TextEditingController txtpass = new TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -19,48 +23,83 @@ class MyApp extends StatelessWidget {
           ),
           centerTitle: true,
         ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Image.asset(
-              "assets/logo.png",
-              height: 75,
-              width: 75,
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Container(
-              padding: EdgeInsets.symmetric(horizontal: 20),
-              child: TextField(
-                decoration: InputDecoration(labelText: "Enter email"),
+        body: Container(
+          margin: EdgeInsets.all(20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Image.asset(
+                "assets/logo.png",
+                height: 75,
+                width: 75,
               ),
-            ),
-            Container(
-              padding: EdgeInsets.symmetric(horizontal: 20),
-              child: TextField(
-                decoration: InputDecoration(labelText: "Enter password"),
+              SizedBox(
+                height: 20,
               ),
-            ),
-            SizedBox(
-              height: 5,
-            ),
-            ElevatedButton(
-              onPressed: () {},
-              child: Text(
-                "Login",
-                style:
-                    TextStyle(fontWeight: FontWeight.bold, color: Colors.black),
+              TextField(
+                controller: txtemail,
+                decoration: InputDecoration(
+                    labelText: "Enter email", icon: Icon(Icons.person)),
               ),
-            ),
-            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                ElevatedButton(onPressed: () {}, child: Text("Forget Password")),
-                ElevatedButton(onPressed: () {}, child: Text("SignUp"))
-              ],
-            )
-          ],
+              TextField(
+                controller: txtpass,
+                decoration: InputDecoration(
+                    labelText: "Enter password", icon: Icon(Icons.lock)),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              ElevatedButton.icon(
+                  onPressed: () {
+                   String text=txtemail.text;
+                   int count=0;
+                   int len=text.length;
+
+                   for (int i=0;i<len;i++){
+                     String char=text[i].toLowerCase();
+                     if(char=='a'||char=='e'||char=='i'||char=='o'||char=='u'){
+                       count++;
+                     }
+                   }
+                   print(count);
+                   if(count%2==0){
+                      print("Success");
+                   }
+                   else {
+                     print("Fail");
+                   }
+
+                   int pass=int.parse(txtpass.text);
+                   int temp=pass;
+                   int sum=0;
+                   int len1=pass.toString().length;
+
+                   while(temp!=0){
+                     int digit=temp%10;
+                     sum +=pow(digit,len1);
+                     temp=(temp/10).toInt();
+                   }
+                   if (sum==num){
+                     print("$pass is an amstrong");
+                   }
+                   else{
+                     print("else not amstrong");
+                   }
+                  },
+                  icon: Icon(Icons.login),
+                  label: Text("Login")),
+              Spacer(),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  ElevatedButton(
+                      onPressed: () {}, child: Text("Forget Password")),
+                  ElevatedButton(onPressed: () {}, child: Text("SignUp"))
+                ],
+              )
+            ],
+          ),
         ));
   }
 }
